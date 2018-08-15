18091bc7e042cf7ef6ff27171b6e6e8906d30d17
wkkkke <1621703096@qq.com>
2018-08-15 14:04:45 +0800

    .

--- lx/vuecli-test/testdemo/src/views/book/book.vue
+++ lx/vuecli-test/testdemo/src/views/book/book.vue
@@ -1,98 +1,49 @@
 <template>
-    <div class="slide">
-        <div class="slideshow">
-            <ul>
-                <li v-for="(img,index)in imgArray" v-show="index==mark" :key="index">
-                    <a href="#"><img src='img'></a>
-                </li>
-            
-            </ul>
+  <div>
+    <div class="container">
+      <swipe class="my-swipe">
+        <swipe-item class="slide1"></swipe-item>
+        <swipe-item class="slide2"></swipe-item>
+        <swipe-item class="slide3"></swipe-item>
+      </swipe>
         </div>
-    
-    <div class="bar">
-        <span v-for="(img,index)in imgArr" :class="{'active':index==mark}" :key="index"></span>
     </div>
-    </div> 
 </template>
 <script>
+require('vue-swipe/dist/vue-swipe.css');
+import { Swipe, SwipeItem } from 'vue-swipe';
 export default {
-    data(){
-        mark:0
-        return{
-           imgArr:[
-               "../img/div4pic1.jpg",
-               "../img/div4pic2.jpg",
-               "../img/div4pic3.jpg"
-           ]
-        }
+  components: {
+    swipe: Swipe,
+    'swipe-item': SwipeItem
     },
-    methods:{
-        autoPlay(){
-            this.mark++;
-            if(mark==3){
-                this.mark=0
+  created() {
+    this.$emit('routerChange','book')
             }
-        
-        },
-        play(){
-            setInterval(this.autoPlay,2500)
-        },
-        change(i){
-            this.mark=i
-        },
-        
-    },
-    created(){
-        this.play()
     }
-}
-</script>
-<style>
 
-  * {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-  }
-  .slide {
-    width: 1024px;
-    height: 320px;
-    margin: 0 auto;
-    margin-top: 50px;
-    overflow: hidden;
-    position: relative;
-  }
-  .slideshow {
-    width: 1024px;
-    height: 320px;
-  }
-  li {
-    position: absolute;
-  }
-  img {
-    width: 1024px;
-    height: 320px;
-  }
-  .bar {
-    position: absolute;
-    width: 100%;
-    bottom: 10px;
-    margin: 0 auto;
-    z-index: 10;
+</script>
+<style scoped>
+.my-swipe {
+  height: 200px;
+  color: #fff;
+  font-size: 30px;
     text-align: center;
   }
-  .bar span {
-    width: 20px;
-    height: 5px;
-    border: 1px solid;
-    background: white;
-    display: inline-block;
-    margin-right: 10px;
-  }
-  .active {
-    background: red !important;
+
+.slide1 {
+  background: url('../../../public/img/div4pic1.jpg') no-repeat center/cover;
+  color: #fff;
   }
 
+.slide2 {
+  background: url('../../../public/img/div4pic2.jpg') no-repeat center/cover;
+  color: #000;
+  }
+
+.slide3 {
+  background: url('../../../public/img/div4pic3.jpg') no-repeat center/cover;
+  color: #fff;
+}
 </style>
-
 
